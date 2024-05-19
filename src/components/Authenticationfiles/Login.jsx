37eb8a502ee4139685abffdf9ec6a856a25b03b8
Sylvia Mumbi <mumbi099@gmail.com>
2024-05-19 13:42:30 +0300

    Passed setUser as prop and used state to handle email and pswd states

--- src/components/Authenticationfiles/Login.jsx
+++ src/components/Authenticationfiles/Login.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import api from '../api/Api';
 
-function Login(){
-  return 
-}
+function Login({ setUser }){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return
+};
+
 export default Login;
