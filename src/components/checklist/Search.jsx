359f35c6e8090fc4e03f492f6b14e761de65c183
mumbi <mumbi099@gmail.com>
2024-07-01 14:51:34 +0300

    Final Updates

--- src/components/checklist/Search.jsx
+++ src/components/checklist/Search.jsx
@@ -22,12 +22,12 @@ function SearchChecklist({ setUser }) {
     }
   };
 
-  const generatePDF = () => {
-    const input = contentRef.current;
+ const generatePDF = () => {
+  const input = contentRef.current;
 
-    html2canvas(input, {
-      scale: 3,
-      ignoreElements: (element) => {
+  html2canvas(input, {
+    scale: 3,
+     ignoreElements: (element) => {
         if (element.classList.contains('search-wrapper')) {
           return true;
         }
@@ -36,7 +36,8 @@ function SearchChecklist({ setUser }) {
         }
         return false;
       },
-    }).then((canvas) => {
+    logging: true, 
+  }).then((canvas) => {
     const imgData = canvas.toDataURL('image/png');
     const pdf = new jsPDF('p', 'px', 'a4');
     const imgWidth = pdf.internal.pageSize.getWidth();
@@ -54,33 +55,33 @@ function SearchChecklist({ setUser }) {
       heightLeft -= pdf.internal.pageSize.getHeight();
     }
 
-    pdf.save('checklist.pdf');
-    });
-  };
+    pdf.save('IT-Checklist.pdf');
+  });
+};
 
   return (
     <div>
-      <div className="search-content-table" ref={contentRef}>
+      <div className="search-content-table">
               <nav className='nav-bar'>
         <img src={logo} alt="logo_image" className='w-[80px] h-auto pointer' onClick={() => navigate('/new-checklist')} />
-        <div className='nav-titles text-xl font-bold'>
+        <div className='nav-titles text-xl font-bold mr-[20px]'>
           <h3>Dakawou Transport Limited</h3>
           <h3>Daily ICT Checklist</h3>
         </div>
       </nav>
       <div >
-        <div className='search-wrapper'>
+        <div className='search-wrapper w-full'>
           <h2 className='search-heading'>Search Checklist by Date</h2>
-          <div className='search-container flex justify-center align-center w-[30%] mt-[15px] justify-around'>
-            <p>Enter date: </p>
+          <div className='search-container flex justify-center align-center mt-[15px] '>
+            <p className='mr-[30px]'>Enter date: </p>
             <input type="date" value={date} onChange={(e) => setDate(e.target.value)} className='date-picker w-[200px] border-2 border-button-color rounded pointer text-center italic placeholder-italic placeholder-gray-400' />
-            <button className='search-button' onClick={handleSearch}>Search</button>
+            <button className='search-button mx-[30px] font-bold' onClick={handleSearch}>Search</button>
           </div>
-          <button className='new-record-btn  bg-button-color border-white text-white h-[35px] w-[170px] rounded-full align-center mb-[25px] transition-transform transform hover:scale-101 transition duration-300 ease-in hover:bg-white hover:text-button-color pointer ease-in absolute right-[1%]' onClick={() => navigate('/new-checklist')}>Create new record</button>
+          <button className='new-record-btn  bg-button-color text-white h-[35px] w-[170px] rounded-full align-center mb-[25px] transition-transform transform hover:scale-101 transition duration-300 ease-in hover:bg-white hover:text-button-color pointer absolute right-[1%] font-bold' onClick={() => navigate('/new-checklist')}>Create new record</button>
         </div>
 
         {checklist && (
-          <div>
+          <div ref={contentRef}>
             <h3 className='h3 text-center color:button-color underline mt-[12px] font-medium'>Checklist for {checklist.date}</h3>
             <table className='search-table mb-[50px]'>
               <thead>
@@ -111,7 +112,7 @@ function SearchChecklist({ setUser }) {
         )}
       </div>
       </div>
-      <button className='generate-pdf-btn bg-button-color border-white text-white h-[35px] w-[130px] rounded-full align-center mb-[25px] transition-transform transform hover:scale-101 transition duration-300 ease-in hover:bg-white hover:text-button-color pointer ease-in' onClick={generatePDF}>Generate PDF</button>
+      <button className='generate-pdf-btn bg-button-color text-white h-[35px] w-[150px] rounded-md  my-[25px] hover:scale-101  hover:bg-white hover:text-button-color pointer ease-in block mx-auto' onClick={generatePDF}>Download Report</button>
     </div>
   );
 }
