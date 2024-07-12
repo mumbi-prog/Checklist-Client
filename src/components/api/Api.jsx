8c17434ce56d49e537c2fbe1f5e595ad48118e8c
mumbi <mumbi099@gmail.com>
2024-07-12 16:47:37 +0300

    Update on Api and Login components

--- src/components/api/Api.jsx
+++ src/components/api/Api.jsx
@@ -1,8 +1,27 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'https://checklist-api-uo6u.onrender.com', 
+//   withCredentials: true,
+// });
+
+// export default api;
+
+// api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://checklist-api-uo6u.onrender.com', 
+  baseURL: 'https://checklist-api-uo6u.onrender.com',
   withCredentials: true,
 });
 
+// Retrieve the token from localStorage
+const token = localStorage.getItem('token');
+
+// Set the token in the axios instance if it exists
+if (token) {
+  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
 export default api;
+
