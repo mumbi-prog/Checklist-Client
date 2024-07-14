a3bc30600afdb38c7c315ea4df606e8d347e5fc8
Sylvia Mumbi <mumbi099@gmail.com>
2024-07-15 01:34:57 +0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -8,13 +8,21 @@ function App() {
   const [user, setUser] = useState(null);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login setUser={setUser} />} />
-        <Route path="/new-checklist" element={<NewChecklist setUser={setUser} />} />
-        <Route path="/search-checklist" element={<SearchChecklist setUser={setUser} />} />
-      </Routes>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Router>
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Login setUser={setUser} />} />
+            <Route path="/new-checklist" element={<NewChecklist setUser={setUser} />} />
+            <Route path="/search-checklist" element={<SearchChecklist setUser={setUser} />} />
+          </Routes>
+        </div>
+        <hr />
+        <footer className="py-2 bg-gray-200 text-center text-xs font-semibold">
+          <p>&copy; 2024 DTL. All rights reserved. Site created by Sylvia Mumbi.</p>
+        </footer>
+      </Router>
+    </div>
   );
 }
 
--- src/components/Authenticationfiles/Login.jsx
+++ src/components/Authenticationfiles/Login.jsx
@@ -1,48 +1,4 @@
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import api from '../api/Api';
-// import './login.css';
 
-// function Login({ setUser }) {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const navigate = useNavigate();
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const response = await api.post('/login', { email, password });
-//       if (response.data.status === 'logged_in') {
-//         setUser(response.data.user);
-//         navigate('/new-checklist');
-//       }
-//     } catch (error) {
-//       console.error('Error logging in:', error);
-//       alert('Invalid email or password');
-//     }
-//   };
-
-//   return (
-//     <div className='form-container bg-button-color cover'>
-//       <form onSubmit={handleSubmit} className='login-form'>
-//         <h2>DTL ICT CHECKLIST</h2>
-//         <div className='d2 block'>
-//           <label className='auth-label block text-sm font-medium text-white-900'>Email:</label>
-//           <input className='mt-[10px] mb-[15px] p-2 w-[300px] rounded-md bg-white focus:outline-none text-sm text-black-900' placeholder='email@dakawou.com' type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-//         </div>
-//         <div className='d2'>
-//           <label className='auth-label block text-sm font-medium text-white-900'>Password:</label>
-//           <input className='mt-[10px] p-2 w-[300px] rounded-md bg-white focus:outline-none text-sm text-black-900' placeholder='Enter password' type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-//         </div>
-//         <button type="submit" className='login-btn'>Login</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
-// Login.jsx
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../api/Api';
